0f944044471beab28d93d45bdf8639811a5e5413
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2018-05-07 12:45:11 +0200

    Revert "cleanup, using matplotlib pdf backend for testing"
    This reverts commit e888979795b7fda6468f83bfc75537150466ffc8.

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,7 +1,7 @@
 import matplotlib as mpl
-# Use pdf backend to prevent matplotlib from creating interactive plots
-# This has to be set before the importing matplotlib.pyplot
-mpl.use('pdf')
+mpl.use('agg')
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 import spharpy
 import numpy as np
