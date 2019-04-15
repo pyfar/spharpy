55f795f1d1a687b56719b154f0414ff2ce561c4c
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-04-15 16:40:04 +0200

    added beamforming module to init of spharpy

--- spharpy/__init__.py
+++ spharpy/__init__.py
@@ -3,7 +3,7 @@ __email__ = 'marco.berzborn@akustik.rwth-aachen.de'
 __version__ = '0.1.2'
 
 from . import samplings
-from . import spherical
 from . import plot
 from . import indexing
 from . import transforms
+from . import beamforming
