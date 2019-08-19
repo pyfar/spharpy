5443daaae4cf456f4772089f940c49220546733d
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-08-19 23:12:19 +0200

    added pcolor on map, refactoring of map plots and contours

--- spharpy/plot/spatial.py
+++ spharpy/plot/spatial.py
@@ -14,6 +14,8 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from matplotlib import cm
 from matplotlib import colors
 
+import cartopy.crs as ccrs
+
 import scipy.spatial as sspat
 from scipy.stats import circmean
 
@@ -472,7 +474,150 @@ def voronoi_cells_sphere(sampling, round_decimals=13):
     ax.set_proj_type('ortho')
 
 
-import cartopy.crs as ccrs
+def _combined_contour(x, y, data, limits, cmap, ax):
+    """Combine a filled contour plot with a black line contour plot for
+    better highlighting.
+
+    Parameters
+    ----------
+    x : ndarray, double
+        The x coordinates.
+    y : ndarray, double
+        The y coordinates.
+    data : ndarray, double
+        The data array.
+    limits : tuple, list
+        Tuple or list containing the maximum and minimum to which the colormap
+        needs to be clipped.
+    cmap : matplotlib.colormap
+        The colormap
+    ax : matplotlib.axes
+        The axes object into which the contour is plotted
+
+    Returns
+    -------
+    cf : matplotlib.ScalarMappable
+        The scalar mappable for the contour plot
+
+    """
+    extend = 'neither'
+    if limits is None:
+        limits = (data.min(), data.max())
+    else:
+        mask_min = data < limits[0]
+        data[mask_min] = limits[0]
+        mask_max = data > limits[1]
+        data[mask_max] = limits[1]
+        if np.any(mask_max) & np.any(mask_min):
+            extend = 'both'
+        elif np.any(mask_max) & ~np.any(mask_min):
+            extend = 'max'
+        elif ~np.any(mask_max) & np.any(mask_min):
+            extend = 'min'
+
+    ax.tricontour(x, y, data, linewidths=0.5, colors='k',
+                  vmin=limits[0], vmax=limits[1], extend=extend)
+    cf = ax.tricontourf(x, y, data, cmap=cmap,
+                        vmin=limits[0], vmax=limits[1], extend=extend)
+
+    return cf
+
+
+def _project_sphere_sampling(latitude, longitude, projection):
+    """Transform the latitude and longitude angles to their corresponding
+    x and y coordinates on a map using the specified map projection.
+
+    Parameters
+    ----------
+    latitude: ndarray, double
+        Geodetic latitude angle of the map in degrees, must be in [-180, 180]
+    longitude: ndarray, double
+        Geodetic longitude angle of the map in degrees, must be in [-90, 90]
+
+    Returns
+    -------
+    x : ndarray, double
+        Array with the x coordinates on the map
+    y : ndarray, double
+        Array with the y coordinates on the map
+
+    """
+    x, y, _ = projection.transform_points(
+        ccrs.PlateCarree(), longitude, latitude).T
+
+    mask = np.invert(np.logical_or(np.isinf(x), np.isinf(y)))
+    x = np.compress(mask, x)
+    y = np.compress(mask, y)
+
+    return x, y
+
+
+def pcolor_map(
+        coordinates,
+        data,
+        projection=ccrs.Mollweide(),
+        limits=None,
+        cmap=cm.viridis,
+        show=True):
+    """
+    Plot the map projection of data points sampled on a spherical surface.
+    The data has to be real.
+
+    Notes
+    -----
+    In case limits are given, all out of bounds data will be clipped to the
+    respective limit.
+
+    Parameters
+    ----------
+    latitude: ndarray, double
+        Geodetic latitude angle of the map, must be in [-pi/2, pi/2]
+    longitude: ndarray, double
+        Geodetic longitude angle of the map, must be in [-pi, pi]
+    data: ndarray, double
+        Data for each angle, must have size corresponding to the number of
+        points given in coordinates.
+    show : boolean, optional
+        Wheter to show the figure or not
+
+    """
+    fig = plt.gcf()
+
+    lat_deg = coordinates.latitude * 180/np.pi
+    lon_deg = coordinates.longitude * 180/np.pi
+    x, y = _project_sphere_sampling(lat_deg, lon_deg, projection)
+    ax = plt.axes(projection=projection)
+
+    ax.set_xlabel('Longitude [$^\\circ$]')
+    ax.set_ylabel('Latitude [$^\\circ$]')
+
+    extend = 'neither'
+    if limits is None:
+        limits = (data.min(), data.max())
+    else:
+        mask_min = data < limits[0]
+        data[mask_min] = limits[0]
+        mask_max = data > limits[1]
+        data[mask_max] = limits[1]
+        if np.any(mask_max) & np.any(mask_min):
+            extend = 'both'
+        elif np.any(mask_max) & ~np.any(mask_min):
+            extend = 'max'
+        elif ~np.any(mask_max) & np.any(mask_min):
+            extend = 'min'
+
+    cf = ax.tripcolor(x, y, data, cmap=cmap, vmin=limits[0], vmax=limits[1])
+
+    ax.relim()
+    ax.autoscale_view()
+
+    ax.gridlines()
+    cb = fig.colorbar(cf, ax=ax, extend=extend)
+    cb.set_label('Amplitude')
+    if show:
+        plt.show()
+
+    return cf
 
 
 def contour_map(
@@ -504,55 +649,24 @@ def contour_map(
         Wheter to show the figure or not
 
     """
+    fig = plt.gcf()
+
     lat_deg = coordinates.latitude * 180/np.pi
     lon_deg = coordinates.longitude * 180/np.pi
-    fig = plt.gcf()
-    proj = projection
-
-    x, y, _ = proj.transform_points(ccrs.PlateCarree(), lon_deg, lat_deg).T
-
-
-    mask = np.invert(np.logical_or(np.isinf(x), np.isinf(y)))
-    x = np.compress(mask, x)
-    y = np.compress(mask, y)
-
-    ax = plt.axes(projection=proj)
+    x, y = _project_sphere_sampling(lat_deg, lon_deg, projection)
+    ax = plt.axes(projection=projection)
 
     ax.set_xlabel('Longitude [$^\\circ$]')
     ax.set_ylabel('Latitude [$^\\circ$]')
 
-    # cf = ax.tripcolor(x, y, (data), cmap=cmap)
+    cf = _combined_contour(x, y, data, limits, cmap, ax)
 
+    ax.relim()
+    ax.autoscale_view()
 
-    cf = ax.tricontourf(x, y, (data), cmap=cmap)
-    cf = ax.tricontour(x, y, (data), colors='k')
-
-    # ax.relim()
-    # ax.autoscale_view()
-
-    # extend = 'neither'
-    # if limits is None:
-    #     limits = (data.min(), data.max())
-    # else:
-    #     mask_min = data < limits[0]
-    #     data[mask_min] = limits[0]
-    #     mask_max = data > limits[1]
-    #     data[mask_max] = limits[1]
-    #     if np.any(mask_max) & np.any(mask_min):
-    #         extend = 'both'
-    #     elif np.any(mask_max) & ~np.any(mask_min):
-    #         extend = 'max'
-    #     elif ~np.any(mask_max) & np.any(mask_min):
-    #         extend = 'min'
-
-    # ax.tricontour(lon_deg, lat_deg, data, linewidths=0.5, colors='k',
-    #               vmin=limits[0], vmax=limits[1], extend=extend)
-    # cf = ax.tricontourf(lon_deg, lat_deg, data, cmap=cmap,
-    #                     vmin=limits[0], vmax=limits[1], extend=extend)
-    #
-    # plt.grid(True)
-    # cb = fig.colorbar(cf, ax=ax)
-    # cb.set_label('Amplitude')
+    ax.gridlines()
+    cb = fig.colorbar(cf, ax=ax)
+    cb.set_label('Amplitude')
     if show:
         plt.show()
 
@@ -589,25 +703,7 @@ def contour(coordinates, data, limits=None, cmap=cm.viridis, show=True):
     ax.set_xlabel('Longitude [$^\\circ$]')
     ax.set_ylabel('Latitude [$^\\circ$]')
 
-    extend = 'neither'
-    if limits is None:
-        limits = (data.min(), data.max())
-    else:
-        mask_min = data < limits[0]
-        data[mask_min] = limits[0]
-        mask_max = data > limits[1]
-        data[mask_max] = limits[1]
-        if np.any(mask_max) & np.any(mask_min):
-            extend = 'both'
-        elif np.any(mask_max) & ~np.any(mask_min):
-            extend = 'max'
-        elif ~np.any(mask_max) & np.any(mask_min):
-            extend = 'min'
-
-    ax.tricontour(lon_deg, lat_deg, data, linewidths=0.5, colors='k',
-                  vmin=limits[0], vmax=limits[1], extend=extend)
-    cf = ax.tricontourf(lon_deg, lat_deg, data, cmap=cmap,
-                        vmin=limits[0], vmax=limits[1], extend=extend)
+    cf = _combined_contour(lon_deg, lat_deg, data, limits, cmap, ax)
 
     plt.grid(True)
     cb = fig.colorbar(cf, ax=ax)
