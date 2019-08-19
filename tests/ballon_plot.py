5dec3e1b52aa1839ad1f0fbb2cc0f7ef6e028984
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-08-19 23:15:47 +0200

    testing scipt

--- tests/ballon_plot.py
+++ tests/ballon_plot.py
@@ -16,28 +16,48 @@ Y = spharpy.spherical.spherical_harmonic_basis_real(15, sampling)
 # spharpy.plot.contour_map(sampling, (Y[:, 4]), cmap=cmap.RdBu_r, projection=ccrs.Mollweide())
 
 
-plt.figure(figsize=(5, 5))
-plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 1], phase=False, cmap=cmap.RdBu_r)
+# plt.figure(figsize=(5, 5))
+# plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 1], phase=False, cmap=cmap.RdBu_r)
 
 
 
-plt.figure(figsize=(5,5))
-plot = spharpy.plot.balloon(sampling, Y[:, 1], phase=False, cmap=cmap.RdBu_r)
+# plt.figure(figsize=(5,5))
+# plot = spharpy.plot.balloon(sampling, Y[:, 1], phase=False, cmap=cmap.RdBu_r)
 
 
-plt.figure(figsize=(5,5))
-plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 7], phase=False, cmap=cmap.viridis)
+# plt.figure(figsize=(5,5))
+# plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 7], phase=False, cmap=cmap.RdBu_r, colorbar=False)
+
+
+# spharpy.plot.contour_map(sampling, Y[:, 7], cmap=cmap.RdBu_r, projection=ccrs.Sinusoidal())
+plt.figure()
+spharpy.plot.contour(sampling, Y[:, 7], cmap=cmap.RdBu_r)
+
+plt.figure()
+spharpy.plot.contour_map(sampling, Y[:, 7], cmap=cmap.RdBu_r)
+
+plt.figure()
+spharpy.plot.pcolor_map(sampling, Y[:, 7], cmap=cmap.RdBu_r, limits=[-0.3, 1])
+
+# plt.figure()
+# spharpy.plot.contour_map(sampling, Y[:, 2], cmap=cmap.RdBu_r, projection=ccrs.Sinusoidal())
+#
+# plt.figure()
+# spharpy.plot.contour_map(sampling, Y[:, 2], cmap=cmap.RdBu_r, projection=ccrs.PlateCarree())
+
+
+
 #
 # plt.figure(figsize=(5,5))
 # spharpy.plot.balloon_interp(sampling, Y[:, 1])
 
-
-Y = spharpy.spherical.spherical_harmonic_basis(15, sampling)
-
-plt.figure(figsize=(5,5))
-plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 1], phase=False)
-
-plt.figure(figsize=(5,5))
-plot = spharpy.plot.balloon(sampling, Y[:, 1], phase=True)
-
-
+#
+# Y = spharpy.spherical.spherical_harmonic_basis(15, sampling)
+#
+# plt.figure(figsize=(5,5))
+# plot = spharpy.plot.balloon_wireframe(sampling, Y[:, 1], phase=False,colorbar=False)
+#
+# plt.figure(figsize=(5,5))
+# plot = spharpy.plot.balloon(sampling, Y[:, 1], phase=True)
+#
+#
