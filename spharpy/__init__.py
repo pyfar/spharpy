a70211f8bb850ac14f4e97e069d733735d73bfd6
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-01-15 15:55:45 +0100

    bugfix missing init spherical

--- spharpy/__init__.py
+++ spharpy/__init__.py
@@ -2,6 +2,7 @@ __author__ = "Marco Berzborn"
 __email__ = 'marco.berzborn@akustik.rwth-aachen.de'
 __version__ = '0.2.0'
 
+from . import spherical
 from . import samplings
 from . import plot
 from . import indexing
