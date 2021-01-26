7f790a76d09c568da203569fdfd991238547da1e
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-01-26 19:23:55 +0100

    working wrapper around scipy's SmoothSphereBivariateSpline

--- spharpy/interpolate.py
+++ spharpy/interpolate.py
@@ -3,16 +3,15 @@ import numpy as np
 
 
 class SmoothSphereBivariateSpline(spinterpolate.SmoothSphereBivariateSpline):
-    def __init__(self, sampling, data, w=None, s=None, eps=1e-16):
+    def __init__(self, sampling, data, w=None, s=1e-4, eps=1e-16):
         theta = sampling.elevation
         phi = sampling.azimuth
-        if s is None:
-            s = sampling.n_points
         if np.any(np.iscomplex(data)):
             raise ValueError("Complex data is not supported.")
         super().__init__(theta, phi, data, w, s, eps)
 
-    def __call__(self, grid, **kwargs):
-        theta = grid.elevation
-        phi = grid.azimuth
-        return super().__call__(theta, phi, **kwargs)
+    def __call__(self, interp_grid, dtheta=0, dphi=0):
+        theta = interp_grid.elevation
+        phi = interp_grid.azimuth
+        return super().__call__(
+            theta, phi, dtheta=dtheta, dphi=dphi, grid=False)
--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -1,15 +1,13 @@
 import spharpy
 from spharpy.samplings import Coordinates
 import spharpy.interpolate as interpolate
-from scipy.interpolate import SmoothSphereBivariateSpline
 import numpy as np
-
-import matplotlib.pyplot as plt
 
 
 def test_smooth_sphere_bivariate_spline_interpolation():
     n_max = 10
-    sampling = spharpy.samplings.equalarea(20, n_points=500, condition_num=np.inf)
+    sampling = spharpy.samplings.equalarea(
+        n_max, n_points=500, condition_num=np.inf)
     Y = spharpy.spherical.spherical_harmonic_basis_real(n_max, sampling)
     y_vec = spharpy.spherical.spherical_harmonic_basis_real(
         n_max, Coordinates(1, 0, 0))
@@ -17,10 +15,15 @@ def test_smooth_sphere_bivariate_spline_interpolation():
 
     data = np.sin(sampling.elevation)*np.sin(2*sampling.azimuth)
 
-    interp_grid = spharpy.samplings.gaussian(15)
+    interp_grid = spharpy.samplings.hyperinterpolation(35)
 
-    weights = np.ones(sampling.n_points)
+    data_grid = np.sin(interp_grid.elevation)*np.sin(2*interp_grid.azimuth)
+
     interpolator = interpolate.SmoothSphereBivariateSpline(
-        sampling, data, s=sampling.n_points)
+        sampling, data, s=1e-4)
 
-    interpolator(interp_grid)
+    interp_data = interpolator(interp_grid)
+
+    # check if error over entire sphere sufficiently small
+    assert np.linalg.norm(np.abs(interp_data - data_grid)) / \
+        np.linalg.norm(np.abs(data_grid)) < 1e-2
