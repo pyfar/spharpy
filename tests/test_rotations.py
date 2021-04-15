8a248f838c0e58387fd551a29c8ad0382991cc62
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-04-15 21:44:15 +0200

    cleanup

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -1,11 +1,9 @@
 """ Tests for sh rotations"""
 
-import pytest
 import numpy as np
 import spharpy.transforms as transforms
 from spharpy.spherical import spherical_harmonic_basis
 from spharpy.samplings import Coordinates
-import spharpy.spherical as spherical
 import spharpy
 
 
@@ -33,9 +31,6 @@ def test_rotation_matrix_z_axis_complex():
 def test_rotation_sh_basis_z_axis_complex():
     rot_angle = np.pi/2
     n_max = 2
-    theta = np.asarray(np.pi/2)[np.newaxis]
-    phi_x = np.asarray(0.0)[np.newaxis]
-    phi_y = np.asarray(np.pi/2)[np.newaxis]
     coords_x = Coordinates(1, 0, 0)
     coords_y = Coordinates(0, -1, 0)
     reference = spherical_harmonic_basis(n_max, coords_x).T.conj()
@@ -50,15 +45,16 @@ def test_rotation_sh_basis_z_axis_complex():
 def test_rotation_maxtrix_z_axis_real():
     n_max = 2
     rot_angle = np.pi/4
-    reference = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, np.cos(rot_angle), 0, np.sin(rot_angle), 0, 0, 0, 0, 0],
-                          [0, 0, 1, 0, 0, 0, 0, 0, 0],
-                          [0, -np.sin(rot_angle), 0, np.cos(rot_angle), 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, np.cos(2*rot_angle), 0, 0, 0, np.sin(2*rot_angle)],
-                          [0, 0, 0, 0, 0, np.cos(rot_angle), 0, np.sin(rot_angle), 0],
-                          [0, 0, 0, 0, 0, 0, 1, 0, 0],
-                          [0, 0, 0, 0, 0, -np.sin(rot_angle), 0, np.cos(rot_angle), 0],
-                          [0, 0, 0, 0, -np.sin(2*rot_angle), 0, 0, 0, np.cos(2*rot_angle)]])
+    reference = np.array([
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, np.cos(rot_angle), 0, np.sin(rot_angle), 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, -np.sin(rot_angle), 0, np.cos(rot_angle), 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, np.cos(2*rot_angle), 0, 0, 0, np.sin(2*rot_angle)],
+        [0, 0, 0, 0, 0, np.cos(rot_angle), 0, np.sin(rot_angle), 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, -np.sin(rot_angle), 0, np.cos(rot_angle), 0],
+        [0, 0, 0, 0, -np.sin(2*rot_angle), 0, 0, 0, np.cos(2*rot_angle)]])
     rot_matrix = transforms.rotation_z_axis_real(n_max, rot_angle)
     np.testing.assert_allclose(rot_matrix, reference)
 
@@ -68,7 +64,8 @@ def test_rotation_sh_basis_z_axis_real():
     n_max = 2
     coords_x = Coordinates(1, 0, 0)
     coords_y = Coordinates(0, -1, 0)
-    reference = np.squeeze(spharpy.spherical.spherical_harmonic_basis_real(n_max, coords_x))
+    reference = np.squeeze(
+        spharpy.spherical.spherical_harmonic_basis_real(n_max, coords_x))
 
     rot_mat = spharpy.transforms.rotation_z_axis_real(n_max, rot_angle)
     sh_vec_x = spharpy.spherical.spherical_harmonic_basis_real(n_max, coords_y)
