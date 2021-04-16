bugfix rotation direction real valued sh

spharpy/transforms/rotations.py
@@ -153,8 +153,8 @@ def wigner_d_rotation_real(n_max, alpha, beta, gamma):
             n, m = spharpy.spherical.acn2nm(col_acn)
             n_dash, m_dash = spharpy.spherical.acn2nm(row_acn)
             if n == n_dash:
-                d_l_1 = wigner_d_function(n, np.abs(m_dash), np.abs(m), beta)
-                d_l_2 = wigner_d_function(n, np.abs(m), -np.abs(m_dash), beta)
+                d_l_1 = wigner_d_function(n, np.abs(m_dash), np.abs(m), -beta)
+                d_l_2 = wigner_d_function(n, np.abs(m), -np.abs(m_dash), -beta)
                 R[row_acn, col_acn] = \
                     sign(m_dash) * Phi(m, alpha) * Phi(m_dash, gamma) * (d_l_1 + (-1)**int(m) * d_l_2)/2 \
                     - sign(m) * Phi(-m, alpha) * Phi(-m_dash, gamma) * (d_l_1 - (-1)**int(m) * d_l_2)/2
@@ -184,7 +184,8 @@ def Phi(m, angle):
     elif m == 0:
         phi = 1
     elif m < 0:
-        phi = np.sqrt(2)*np.sin(np.abs(m)*angle)
+        phi = -np.sqrt(2)*np.sin(np.abs(m)*angle)
+        # phi = np.sqrt(2)*np.sin(np.abs(m)*angle)*(-1.)**(m+1)
 
     return phi
 

tests/test_rotations.py
@@ -79,7 +79,7 @@ def test_rotation_sh_basis_z_axis_real():
 
 def test_wigner_d_rot_real():
     euler_angles = []
-    n_max = 2
+    n_max = 5
     euler_angles = np.deg2rad(np.array([90., 45., -90.]))
     D = spharpy.transforms.wigner_d_rotation_real(
         n_max, euler_angles[0], euler_angles[1], euler_angles[2])
@@ -91,7 +91,7 @@ def test_wigner_d_rot_real():
         n_max, Coordinates(0, 1, 0)))
     sh_vec_rotated = D @ sh_vec
 
-    np.testing.assert_allclose(sh_vec_rotated, reference)
+    np.testing.assert_allclose(sh_vec_rotated, reference, atol=1e-10)
 
     rot_angle_z = np.pi/4
     D = spharpy.transforms.wigner_d_rotation_real(
@@ -106,7 +106,7 @@ def test_wigner_d_rot_real():
 
 def test_wigner_d_rot():
     euler_angles = []
-    n_max = 2
+    n_max = 5
     euler_angles = np.deg2rad(np.array([90., 45., -90.]))
     D = spharpy.transforms.wigner_d_rotation(
         n_max, euler_angles[0], euler_angles[1], euler_angles[2])