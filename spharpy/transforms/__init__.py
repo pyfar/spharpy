f46cceb72350d02be2749bc6f0a49aad610b84ba
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-04-19 12:34:05 +0200

    add wigner-d function to rotations init

--- spharpy/transforms/__init__.py
+++ spharpy/transforms/__init__.py
@@ -1,6 +1,7 @@
 from .rotations import (
     rotation_z_axis,
     rotation_z_axis_real,
+    wigner_d_function,
     wigner_d_rotation,
     wigner_d_rotation_real)
 
@@ -9,4 +10,5 @@ __all__ = [
     'rotation_z_axis',
     'rotation_z_axis_real',
     'wigner_d_rotation',
-    'wigner_d_rotation_real']
+    'wigner_d_rotation_real',
+    'wigner_d_function']
