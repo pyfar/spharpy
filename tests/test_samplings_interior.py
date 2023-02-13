6d20fe6d1659fef9672fed5a85b638c393cf114c
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2023-02-13 15:00:20 +0100

    minor refactoring interior points calculation

--- tests/test_samplings_interior.py
+++ tests/test_samplings_interior.py
@@ -1,7 +1,5 @@
-import pytest
 import numpy as np
 import spharpy.samplings as samplings
-from spharpy.samplings.coordinates import Coordinates, SamplingSphere
 from spharpy.special import spherical_bessel_zeros
 from scipy.special import spherical_jn
 
@@ -9,11 +7,10 @@ from scipy.special import spherical_jn
 def test_sph_bessel_zeros():
     roots = spherical_bessel_zeros(3, 3)
     jn_zeros = np.ones(roots.shape)
-    for n in range(0, 4):
+    for n in range(4):
         jn_zeros[n, :] = spherical_jn(n, roots[n, :])
-    zeros = np.zeros((4, 3), dtype=np.float)
+    zeros = np.zeros((4, 3), dtype=float)
     np.testing.assert_allclose(jn_zeros, zeros, atol=1e-12)
-
 
 
 def test_interior_points_chardon():
@@ -21,6 +18,6 @@ def test_interior_points_chardon():
     int_points = samplings.interior_stabilization_points(kr_max)
 
     filename = 'tests/data/interior_points_kr7.csv'
-    truth = np.genfromtxt(filename, dtype=np.double, delimiter=';')
+    truth = np.genfromtxt(filename, dtype=float, delimiter=';')
 
     np.testing.assert_allclose(int_points.cartesian.T, truth, atol=1e-7)
