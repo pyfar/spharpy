2edb272acae014199e91d62d2ca4c762afd902b3
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2023-02-13 15:12:01 +0100

    require numpy < v1.20

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'numpy>=1.10',
+    'numpy<1.20',
     'scipy',
     'urllib3',
     'matplotlib'
