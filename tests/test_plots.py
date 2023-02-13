a9ffc5cb2a1eda20b75b493e6330f08339318283
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2023-02-13 17:59:08 +0100

    simplify test data

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -13,32 +13,20 @@ from spharpy import plot
 
 
 def test_balloon_plot_abs():
-    n_max = 1
     coords = spharpy.samplings.icosahedron()
-    doa = spharpy.samplings.Coordinates(1, 0, 0)
-    y_vec = spharpy.spherical.spherical_harmonic_basis_real(n_max, doa)
-    basis = spharpy.spherical.spherical_harmonic_basis_real(n_max, coords)
-    data = np.squeeze(4*np.pi/(n_max+1)**2 * basis @ y_vec.T)
+    data = np.cos(coords.azimuth)*np.sin(coords.elevation)
     spharpy.plot.balloon(coords, data, show=False)
 
 
 def test_balloon_plot_phase():
-    n_max = 1
     coords = spharpy.samplings.hyperinterpolation(10)
-    doa = spharpy.samplings.Coordinates(1, 0, 0)
-    y_vec = spharpy.spherical.spherical_harmonic_basis_real(n_max, doa)
-    basis = spharpy.spherical.spherical_harmonic_basis_real(n_max, coords)
-    data = np.squeeze(4*np.pi/(n_max+1)**2 * basis @ y_vec.T)
+    data = np.cos(coords.azimuth)*np.sin(coords.elevation)
     spharpy.plot.balloon(coords, data, phase=True, show=False)
 
 
 def test_contour_plot():
-    n_max = 1
     coords = spharpy.samplings.hyperinterpolation(10)
-    doa = spharpy.samplings.Coordinates(1, 0, 0)
-    y_vec = spharpy.spherical.spherical_harmonic_basis_real(n_max, doa)
-    basis = spharpy.spherical.spherical_harmonic_basis_real(n_max, coords)
-    data = np.squeeze(4*np.pi/(n_max+1)**2 * basis @ y_vec.T)
+    data = np.cos(coords.azimuth)*np.sin(coords.elevation)
     spharpy.plot.contour(coords, np.abs(data), show=False)
 
 
