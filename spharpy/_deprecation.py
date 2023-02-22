27184dabac9616996f44d00423acc49bcb6ff23f
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2023-02-22 15:57:33 +0100

    cleanup

--- spharpy/_deprecation.py
+++ spharpy/_deprecation.py
@@ -1,21 +1,13 @@
-import warnings
-from spharpy.samplings import Coordinates as SphCoordinates
-from spharpy.samplings import SamplingSphere as SphSamplingSphere
+from spharpy.samplings import Coordinates
+from spharpy.samplings import SamplingSphere
 import pyfar as pf
+
 
 def convert_coordinates(coordinates):
     coords_type = type(coordinates)
-    if coords_type is pf.Coordinates:
-        if coordinates.sh_order is None:
-            return SphCoordinates.from_pyfar(coordinates)
-        else:
-            return SphSamplingSphere.from_pyfar(coordinates)
-    else:
+    if coords_type is not pf.Coordinates:
         return coordinates
-    # if coords_type is not SphCoordinates and coords_type is not SphSamplingSphere:
-    #     return coordinates
-    # warnings.warn(
-    #     'The use of spharpy Coordinates will be deprecated soon. '
-    #     'Use pyfar.Coordinates instead',
-    #     DeprecationWarning)
-    return coordinates.to_pyfar()
+    if coordinates.sh_order is None:
+        return Coordinates.from_pyfar(coordinates)
+    else:
+        return SamplingSphere.from_pyfar(coordinates)
