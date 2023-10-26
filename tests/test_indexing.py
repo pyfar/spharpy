7861ff33108d8e5778bc07ea1c4d0ff80108290e
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2023-10-26 17:28:28 +0200

    move content of indexing to the spherical submodule

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,27 +1,28 @@
 """ Tests for sh channel indexing"""
 
-import pytest
 import numpy as np
-import spharpy.indexing
+import spharpy
+
 
 def test_identity_matrix_n_nm():
     reference = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
                           [0, 1, 1, 1, 0, 0, 0, 0, 0],
                           [0, 0, 0, 0, 1, 1, 1, 1, 1]])
 
-    identity = spharpy.indexing.sph_identity_matrix(2, type='n-nm')
+    identity = spharpy.spherical.sph_identity_matrix(2, type='n-nm')
     np.testing.assert_allclose(reference, identity)
+
 
 def test_sid_indexing():
     n_max = 2
     reference_n = np.array([0, 1, 1, 1, 2, 2, 2, 2, 2])
     reference_m = np.array([0, 1, -1, 0, 2, -2, 1, -1, 0])
 
-
-    sid_n, sid_m = spharpy.indexing.sid(n_max)
+    sid_n, sid_m = spharpy.spherical.sid(n_max)
 
     np.testing.assert_equal(reference_n, sid_n)
     np.testing.assert_equal(reference_m, sid_m)
+
 
 def test_sid2acn():
     n_max = 2
@@ -29,5 +30,5 @@ def test_sid2acn():
     # with indexing starting at 1.
     reference_acn = np.array([1, 3, 4, 2, 6, 8, 9, 7, 5]) - 1
 
-    acn_indeces = spharpy.indexing.sid2acn(n_max)
-    np.testing.assert_equal(reference_acn, acn_indeces)
+    acn_indices = spharpy.spherical.sid2acn(n_max)
+    np.testing.assert_equal(reference_acn, acn_indices)
