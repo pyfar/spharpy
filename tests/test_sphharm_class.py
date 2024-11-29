8686745dbc3a842ffb3132d1fee8e22af14c8220
Xenofon Karakonstantis <xeka@trackman.com>
2024-11-29 12:52:36 +0100

    Update phase_convention parameter to accept string or None as discussed

--- tests/test_sphharm_class.py
+++ tests/test_sphharm_class.py
@@ -7,38 +7,32 @@ import pyfar as pf
 from spharpy.spherical import SphericalHarmonics
 from spharpy.samplings import gaussian
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_init():
     coordinates = pf.Coordinates(1, 0, 0)
     sph_harm = SphericalHarmonics(n_max=2, coordinates=coordinates)
     assert sph_harm.n_max == 2
     assert np.all(sph_harm.coordinates == coordinates)
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_init_invalid_coordinates():
     with pytest.raises(TypeError):
         SphericalHarmonics(n_max=2, coordinates=[0, 0, 1])
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_init_invalid_n_max():
     coordinates = pf.Coordinates(1, 0, 0)
     with pytest.raises(ValueError):
         SphericalHarmonics(n_max=-1, coordinates=coordinates)
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_compute_basis():
     coordinates = gaussian(n_points=8)
     sph_harm = SphericalHarmonics(n_max=2, coordinates=coordinates)
     assert sph_harm.basis is not None
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_compute_basis_gradient():
     coordinates = pf.Coordinates(1, 0, 0)
     sph_harm = SphericalHarmonics(n_max=2, coordinates=coordinates)
     assert sph_harm.basis_gradient_theta is not None
     assert sph_harm.basis_gradient_phi is not None
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_compute_inverse_quad():
     coordinates = gaussian(n_points=4)
     # coordinates = pf.Coordinates(1, 0, 0)
@@ -46,7 +40,6 @@ def test_sphharm_compute_inverse_quad():
     sh = SphericalHarmonics(2, coordinates, inverse_transform = 'quadrature')
     assert sh.basis_inv is not None
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_sphharm_compute_inverse_pseudo_inv():
     coordinates = gaussian(n_points= 5)
     # coordinates = pf.Coordinates(1, 0, 0)
@@ -54,7 +47,6 @@ def test_sphharm_compute_inverse_pseudo_inv():
     sh = SphericalHarmonics(2, coordinates, inverse_transform = 'pseudo_inverse')
     assert sh.basis_inv is not None
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
 def test_compute_basis_caching():
     n_max = 2
     points = np.random.rand(10, 3)
