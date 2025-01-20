990dc1eb42ca1a154980b1f3c2d4c4e74bcfec27
Tim Lübeck <tim.luebeck@icloud.com>
2025-01-20 14:26:18 +0100

    add test

---
+++ tests/test_normalization_conversion.py
@@ -0,0 +1,10 @@
+import spharpy.spherical as sh
+import numpy as np
+
+
+def test_n3d2_to_maxn():
+    raise NotImplementedError()
+
+
+def test_n3d2_to_sn3d_norm():
+    raise NotImplementedError()
