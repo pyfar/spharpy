b3d628320c6035b33e2379cfe798939fd8cd1e18
Tim Lübeck <tim.luebeck@icloud.com>
2025-05-06 14:13:14 +0200

    update SHSignal accordingly, edit docstrings according to review

--- spharpy/classes/audio.py
+++ spharpy/classes/audio.py
@@ -1,6 +1,7 @@
 from pyfar import Signal
 from spharpy.spherical import n3d_to_maxn, n3d_to_sn3d_norm
 from spharpy.spherical import fuma_to_nm, nm_to_fuma, acn_to_nm, nm_to_acn
+from spharpy.spherical import renormalize, change_channel_convention
 import numpy as np
 
 
@@ -18,67 +19,67 @@ class SphericalHarmonicSignal(Signal):
     the Condon-Shortley phase.
 
 
-        Parameters
-        ----------
-        data : ndarray, double
-            Raw data of the spherical harmonics signal in the time or
-            frequency domain. The memory layout of data is 'C'. E.g.
-            data of ``shape = (3, 4, 1024)`` has 3 channels with 4
-            spherical harmonics coefficients with 1024 samples or frequency
-            bins each. Time data is converted to ``float``. Frequency is
-            converted to ``complex`` and must be provided as single
-            sided spectra, i.e., for all frequencies between 0 Hz and
-            half the sampling rate.
-        sampling_rate : double
-            Sampling rate in Hz
-        n_max : int
-            Maximum spherical harmonic order. Has to match the number of
-            coefficients, such that the number of coefficients
-            .. math:: >= (n_max + 1) ** 2.
-        basis_type : str
-            Type of spherical harmonic basis, either ``'complex'`` or
-            ``'real'``. The default is ``'real'``.
-        normalization : str
-            Normalization convention, either ``'n3d'``, ``'maxN'`` or
-            ``'sn3d'``. The default is ``'n3d'``.
-            (maxN is only supported up to 3rd order)
-        channel_convention : str
-            Channel ordering convention, either ``'acn'`` or ``'fuma'``.
-            The default is ``'acn'``.
-            (FuMa is only supported up to 3rd order)
-        condon_shortley : bool or str, optional
-            Whether to include the Condon-Shortley phase term. If ``True`` or
-            ``'auto'``, Condon-Shortley is included, if ``False`` it is not
-            included. The default is ``'auto'``.
-        n_samples : int, optional
-            Number of samples of the time signal. Required if domain is
-            ``'freq'``. The default is ``None``, which assumes an even number
-            of samples if the data is provided in the frequency domain.
-        domain : ``'time'``, ``'freq'``, optional
-            Domain of data. The default is ``'time'``
-        fft_norm : str, optional
-            The normalization of the Discrete Fourier Transform (DFT). Can be
-            ``'none'``, ``'unitary'``, ``'amplitude'``, ``'rms'``, ``'power'``,
-            or ``'psd'``. See :py:func:`~pyfar.dsp.fft.normalization` and [#]_
-            for more information. The default is ``'none'``, which is typically
-            used for energy signals, such as impulse responses.
-        comment : str
-            A comment related to `data`. The default is ``None``.
-        is_complex : bool, optional
-            Specifies if the underlying time domain data are complex
-            or real-valued. If ``True`` and `domain` is ``'time'``, the
-            input data will be cast to complex. The default is ``False``.
+    Parameters
+    ----------
+    data : ndarray, double
+        Raw data of the spherical harmonics signal in the time or
+        frequency domain. The memory layout of data is 'C'. E.g.
+        data of ``shape = (3, 4, 1024)`` has 3 channels with 4
+        spherical harmonic coefficients with 1024 samples or frequency
+        bins each. Time data is converted to ``float``. Frequency is
+        converted to ``complex`` and must be provided as single
+        sided spectra, i.e., for all frequencies between 0 Hz and
+        half the sampling rate.
+    sampling_rate : double
+        Sampling rate in Hz
+    n_max : int
+        Maximum spherical harmonic order. Has to match the number of
+        coefficients, such that the number of coefficients
+        .. math:: >= (n_max + 1) ** 2.
+    basis_type : str
+        Type of spherical harmonic basis, either ``'complex'`` or
+        ``'real'``. The default is ``'real'``.
+    normalization : str
+        Normalization convention, either ``'n3d'``, ``'maxN'`` or
+        ``'sn3d'``. The default is ``'n3d'``.
+        (maxN is only supported up to 3rd order)
+    channel_convention : str
+        Channel ordering convention, either ``'acn'`` or ``'fuma'``.
+        The default is ``'acn'``.
+        (FuMa is only supported up to 3rd order)
+    condon_shortley : bool or str, optional
+        Whether to include the Condon-Shortley phase term. If ``True`` or
+        ``'auto'``, Condon-Shortley is included, if ``False`` it is not
+        included. The default is ``'auto'``.
+    n_samples : int, optional
+        Number of time domain samples. Required if domain is ``'freq'``.
+        The default is ``None``, which assumes an even number of samples 
+        if the data is provided in the frequency domain.
+    domain : ``'time'``, ``'freq'``, optional
+        Domain of data. The default is ``'time'``
+    fft_norm : str, optional
+        The normalization of the Discrete Fourier Transform (DFT). Can be
+        ``'none'``, ``'unitary'``, ``'amplitude'``, ``'rms'``, ``'power'``,
+        or ``'psd'``. See :py:func:`~pyfar.dsp.fft.normalization` and [#]_
+        for more information. The default is ``'none'``, which is typically
+        used for energy signals, such as impulse responses.
+    comment : str
+        A comment related to `data`. The default is ``None``.
+    is_complex : bool, optional
+        Specifies if the underlying time domain data are complex
+        or real-valued. If ``True`` and `domain` is ``'time'``, the
+        input data will be cast to complex. The default is ``False``.
 
-        References
-        ----------
-        .. [#] E.G. Williams, "Fourier Acoustics", (1999), Academic Press
-        .. [#] B. Rafely, "Fundamentals of Spherical Array Processing", (2015),
-               Springer-Verlag
-        .. [#] F. Zotter, M. Frank, "Ambisonics A Practical 3D Audio Theory
-               for Recording, Studio Production, Sound Reinforcement, and
-               Virtual Reality", (2019), Springer-Verlag
+    References
+    ----------
+    .. [#] E.G. Williams, "Fourier Acoustics", (1999), Academic Press
+    .. [#] B. Rafely, "Fundamentals of Spherical Array Processing", (2015),
+            Springer-Verlag
+    .. [#] F. Zotter, M. Frank, "Ambisonics A Practical 3D Audio Theory
+            for Recording, Studio Production, Sound Reinforcement, and
+            Virtual Reality", (2019), Springer-Verlag
 
-        """
+    """
     def __init__(
             self,
             data,
@@ -127,7 +128,7 @@ class SphericalHarmonicSignal(Signal):
                              f"or 'fuma', but is {channel_convention}")
         self._channel_convention = channel_convention
 
-        # set condon shortley
+        # set Condon Shortley
         if not isinstance(condon_shortley, bool) and condon_shortley != 'auto':
             raise ValueError(
                 "Condon_shortley has to be a bool, or 'auto'.")
@@ -143,24 +144,27 @@ class SphericalHarmonicSignal(Signal):
 
     @property
     def n_max(self):
-        """the maximum Spherical Harmonics order."""
+        """the maximum spherical harmonic order."""
         return self._n_max
 
     @property
     def basis_type(self):
-        """the type of the Spherical Harmonics bases (complex or real)."""
+        """the type of the spherical harmonic basis (``complex`` or ``real``).
+        """
         return self._basis_type
 
     @property
     def normalization(self):
-        """the normalization of the Spherical Harmonics coefficients."""
+        """the normalization of the spherical harmonic coefficients."""
         return self._normalization
 
     @normalization.setter
     def normalization(self, value):
-        """set the normalization of the Spherical Harmonics coefficients."""
+        """set the normalization of the spherical harmonic coefficients."""
         if self.normalization is not value:
-            self._renormalize(value)
+            self._data = renormalize(self._data, self.channel_convention,
+                                     self.normalization, value, axis=-2)
+            self._normalization = value
 
     @property
     def condon_shortley(self):
@@ -168,65 +172,20 @@ class SphericalHarmonicSignal(Signal):
 
     @property
     def channel_convention(self):
-        """the channel convention of the Spherical Harmonics coefficients."""
+        """the channel convention of the spherical harmonic coefficients."""
         return self._channel_convention
 
     @channel_convention.setter
     def channel_convention(self, value):
         """
-        set the channel convention of the Spherical Harmonics coefficients.
+        set the channel convention of the spherical harmonic coefficients.
         """
         if value not in ["acn", "fuma"]:
             raise ValueError("Invalid channel convention, has to be 'acn' "
                              f"or 'fuma', but is {value}")
 
         if self.channel_convention is not value:
-            self._change_channel_convention()
-
-    def _renormalize(self, value):
-        if value not in ["n3d", "maxN", "sn3d"]:
-            raise ValueError("Invalid normalization, has to be 'sn3d', "
-                             f"'n3d', or 'maxN', but is {value}")
-        acn = np.arange((self.n_max + 1) ** 2)
-
-        if self.channel_convention == "fuma":
-            orders, _ = fuma_to_nm(acn)
-        else:
-            orders, _ = acn_to_nm(acn)
-
-        if self._normalization == 'n3d':
-            if value == "sn3d":
-                self._data[:, :, ...] *= \
-                    n3d_to_sn3d_norm(orders)[np.newaxis].T
-            elif value == "maxN":
-                self._data[:, :, ...] *= \
-                    n3d_to_maxn(acn)[np.newaxis].T
-
-        if self._normalization == 'sn3d':
-            # convert to n3d
-            self._data[:, :, ...] /= \
-                    n3d_to_sn3d_norm(orders)[np.newaxis].T
-            if value == "maxN":
-                self._data[:, :, ...] *= n3d_to_maxn(acn)[np.newaxis].T
-
-        if self._normalization == 'maxN':
-            # convert to n3d
-            self._data[:, :, ...] /= \
-                    n3d_to_maxn(acn)[np.newaxis].T
-            if value == "sn3d":
-                self._data[:, :, ...] *= \
-                    n3d_to_sn3d_norm(orders)[np.newaxis].T
-        self._normalization = value
-
-    def _change_channel_convention(self):
-        acn = np.arange((self.n_max + 1) ** 2)
-        if self._channel_convention == 'acn':
-            self._channel_convention = 'fuma'
-            n, m = acn_to_nm(acn)
-            idx = nm_to_fuma(n, m)
-        elif self._channel_convention == 'fuma':
-            self._channel_convention = 'acn'
-            n, m = fuma_to_nm(acn)
-            idx = nm_to_acn(n, m)
-
-        self._data = self._data[idx, ...]
+            self._data = change_channel_convention(self._data,
+                                                   self.channel_convention,
+                                                   value, axis=-2)
+            self._channel_convention = value
