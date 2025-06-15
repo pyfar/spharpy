58efb437711f18ccaf28ec8aebf424db14ce318d
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-06-15 18:02:43 +0200

    remove print

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -55,7 +55,6 @@ for file in os.listdir(output_path):
     (sp.plot.pcolor_sphere)])
 def test_spherical_default(function):
     """Test all spherical plots with default arguments."""
-    logger.info(f"Testing: {function.__name__}")
     coords = sp.samplings.equal_area(n_max=0, n_points=500)
     data = np.sin(coords.colatitude) * np.cos(coords.azimuth)
 
