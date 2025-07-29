93f1fb3bf837fcae04d2509ee2406b2a1f2366d1
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-07-29 17:30:48 +0200

    Apply suggestions from code review

--- spharpy/samplings/helpers.py
+++ spharpy/samplings/helpers.py
@@ -68,9 +68,6 @@ def spherical_voronoi(sampling, round_decimals=13, center=0.0):
         Spherical voronoi diagram as implemented in scipy.
 
     """
-    if not isinstance(sampling, pf.Coordinates):
-        raise ValueError("sampling must be a coordinates object.")
-    
     points = sampling.cartesian
     points = points if points.shape[-1] == 3 else points.T
     radius = np.unique(np.round(sampling.radius, decimals=round_decimals))
