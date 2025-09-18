253986a725b0d56fbd28d13676ee4cc7d78e7dfa
Anton Hoyer <156099087+hoyer-a@users.noreply.github.com>
2025-09-18 10:20:32 +0200

    harmonize contour / contour_map; bugfixes

--- spharpy/plot/spatial.py
+++ spharpy/plot/spatial.py
@@ -678,7 +678,7 @@ def voronoi_cells_sphere(sampling, round_decimals=13, ax=None):
     return ax
 
 
-def _combined_contour(x, y, data, limits, cmap, ax):
+def _combined_contour(x, y, data, limits, cmap, levels, ax):
     """Combine a filled contour plot with a black line contour plot for
     better highlighting.
 
@@ -695,6 +695,13 @@ def _combined_contour(x, y, data, limits, cmap, ax):
         needs to be clipped.
     cmap : :py:class:`matplotlib.colors.Colormap`
         Colormap for the plot, see matplotlib.cm.
+    levels : int or array-like, optional
+        Determines the number and positions of the contour lines / regions.
+        If an int n, use :py:class:`matplotlib.ticker.MaxNLocator`,
+        which tries to automatically choose
+        no more than n+1 "nice" contour levels between minimum and maximum
+        numeric values of Z. If array-like, draw contour lines at the
+        specified levels. The values must be in increasing order.
     ax : matplotlib.axes
         The axes object into which the contour is plotted
 
@@ -719,10 +726,11 @@ def _combined_contour(x, y, data, limits, cmap, ax):
         elif ~np.any(mask_max) & np.any(mask_min):
             extend = 'min'
 
-    ax.tricontour(x, y, data, linewidths=0.5, colors='k',
+    ax.tricontour(x, y, data, levels=levels, linewidths=0.5, colors='k',
                   vmin=limits[0], vmax=limits[1], extend=extend)
     return ax.tricontourf(
-        x, y, data, cmap=cmap, vmin=limits[0], vmax=limits[1], extend=extend)
+        x, y, data, levels=levels, cmap=cmap, vmin=limits[0], vmax=limits[1],
+        extend=extend)
 
 
 def pcolor_map(
@@ -952,8 +960,11 @@ def contour_map(
 
     ax.contour(xi, yi, zi, levels=levels, linewidths=0.5, colors='k',
                vmin=limits[0], vmax=limits[1], extend=extend)
-    cf = ax.pcolormesh(xi, yi, zi, cmap=cmap, shading='gouraud',
-                       vmin=limits[0], vmax=limits[1])
+    cf = ax.contourf(xi, yi, zi, levels=levels, cmap=cmap, vmin=limits[0],
+                     vmax=limits[1])
+
+    if type(levels) is int:
+        levels = mpl.ticker.MaxNLocator(levels)
 
     plt.grid(True)
     if colorbar:
@@ -969,6 +980,7 @@ def contour(
         cmap=plt.get_cmap('viridis'),
         colorbar=True,
         limits=None,
+        levels=None,
         ax=None):
     """
     Plot the map projection of data points sampled on a spherical surface.
@@ -994,6 +1006,13 @@ def contour(
         Tuple or list containing the maximum and minimum to which the colormap
         needs to be clipped. If `None`, the limits are set to the minimum and
         maximum of the data.
+    levels : int or array-like, optional
+        Determines the number and positions of the contour lines / regions.
+        If an int n, use :py:class:`matplotlib.ticker.MaxNLocator`,
+        which tries to automatically choose
+        no more than n+1 "nice" contour levels between minimum and maximum
+        numeric values of Z. If array-like, draw contour lines at the
+        specified levels. The values must be in increasing order.
     ax : matplotlib.axis, None, optional
         The matplotlib axis object used for plotting. By default `None`, which
         will create a new axis object with the specified projection.
@@ -1019,6 +1038,7 @@ def contour(
     """
     # input checks
     _check_input_parameters(coordinates, data, cmap, colorbar, limits)
+    data = data.copy()
 
     height, latitude, longitude = coordinates2latlon(coordinates)
     lat_deg = latitude * 180/np.pi
@@ -1035,11 +1055,14 @@ def contour(
     ax.set_xlabel('Longitude [$^\\circ$]')
     ax.set_ylabel('Latitude [$^\\circ$]')
 
-    cf = _combined_contour(lon_deg, lat_deg, data, limits, cmap, ax)
+    cf = _combined_contour(lon_deg, lat_deg, data, limits, cmap, levels, ax)
+
+    if type(levels) is int:
+        levels = mpl.ticker.MaxNLocator(levels)
 
     plt.grid(True)
     if colorbar:
-        cb = fig.colorbar(cf, ax=ax)
+        cb = fig.colorbar(cf, ax=ax, ticks=levels)
         cb.set_label('Amplitude')
 
     return ax, cf
