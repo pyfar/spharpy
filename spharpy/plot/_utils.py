97fb68905a903dc4dcb0388998ef6c45beb2b510
Anton Hoyer <156099087+hoyer-a@users.noreply.github.com>
2025-10-21 17:18:35 +0200

    add utils functions for plots

---
+++ spharpy/plot/_utils.py
@@ -0,0 +1,82 @@
+"""Private utility functions for plot module."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def _add_colorbar(colorbar, fig, ax, mappable, label):
+    """
+    Add colorbar to plot
+
+    Parameters
+    ----------
+
+    colorbar : bool
+        Flag indicating if a colobar should be added to the plot
+    fig : matplotlib.figure.Figure
+        Figure to plot on.
+    ax : list[matplotlib.axes.Axes]
+        Either a list of two axes objects or a list with one axis and None
+        object.
+    mappable : matplotlib.cm.ScalarMappable
+        The matplotlib.cm.ScalarMappable described by the colorbar.
+    label : string
+        colorbar label
+
+    Returns
+    -------
+    cb : matplotlib colorbar object
+    """
+    if colorbar:
+        if ax[1] is None:
+            cb = fig.colorbar(mappable, ax=ax[0])
+        else:
+            cb = fig.colorbar(mappable, ax=ax[1])
+        cb.set_label(label)
+    else:
+        cb = None
+
+    return cb
+
+
+def _prepare_plot(ax, projection=None):
+    """
+    Returns a figure to plot on.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes or list, tuple or ndarray of maplotlib.axes.Axes
+        Axes to plot on. The default is None in which case the axes are
+        obtained from the current figure. A new figure is created if it does
+        not exist.
+    projection : str, optional
+        Type of projection for the axes. This is only applied if new axes are
+        created. Default is ``None`` (2D projection).
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+        Returns the active figure.
+    ax : maptlotlib.axes.Axes
+        Returns the current axes.
+    """
+    if ax is None:
+        # get current figure or create a new one
+        fig = plt.gcf()
+        if fig.axes:
+            ax = plt.gca()
+        else:
+            ax = plt.axes(projection=projection)
+
+    else:
+        # get figure from axis
+        # ax can be array or tuple of two ax objects
+        # first axis for the plot, second axis for colorbar placement
+        if isinstance(ax, np.ndarray):
+            fig = ax.flatten()[0].figure
+        elif isinstance(ax, (list, tuple)):
+            fig = ax[0].figure
+        else:
+            fig = ax.figure
+
+    return fig, ax
---
+++ tests/test_plot__utils.py
@@ -0,0 +1,45 @@
+import pytest
+import spharpy as sp
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+@pytest.mark.parametrize(
+    ("input_ax", "output_type", "projection"),
+    [(None, plt.Axes, '3d'), (plt.gca(), plt.Axes, None),
+     ([plt.gca(), plt.gca()], list, None)]
+)
+def test_prepare_plot(input_ax, output_type, projection):
+    """
+    Test output of :py:func:`~spharpy.plot._utils._prepare_plot`.
+    """
+    fig, ax = sp.plot._prepare_plot(input_ax, projection)
+
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(ax, output_type)
+
+    if isinstance(ax, list):
+        assert all(isinstance(ax_, plt.Axes) for ax_ in input_ax)
+
+    if ax is None:
+        assert ax.name == projection
+
+
+@pytest.mark.parametrize(
+        ("colorbar", "ax", "return_type"),
+        [(True, [plt.axes(), None], mpl.colorbar.Colorbar),
+         (True, [plt.axes(), plt.axes()], mpl.colorbar.Colorbar),
+         (False, [plt.axes(), None], None)]
+)
+def test_add_colorbar(colorbar, ax, return_type):
+    norm = mpl.colors.Normalize(vmin=0, vmax=10)
+    cmap = plt.get_cmap('viridis')
+    mappable = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    fig = plt.gcf()
+    cb = sp.plot._utils._add_colorbar(colorbar, fig, ax, mappable, None)
+
+    if return_type is None:
+        assert cb is None
+    else:
+        assert isinstance(cb, return_type)
