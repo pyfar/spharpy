c7e94cefa542b577afe29638b028378b3a97eb96
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-10-22 14:49:48 +0200

    Update spharpy/samplings/samplings.py
    Co-authored-by: Marco Berzborn <mberz@users.noreply.github.com>

--- spharpy/samplings/samplings.py
+++ spharpy/samplings/samplings.py
@@ -92,8 +92,8 @@ def hyperinterpolation(n_max, radius=1.):
     Return a Hyperinterpolation sampling grid.
 
     After Sloan and Womersley [#]_. The samplings are available for
-    spherical harmonics orders :math:`1 \leq n_\text{max} \leq 200` and the number
-    of points in the sampling grird equals :math:`(n_\text{max} + 1)^2`.
+    spherical harmonics orders :math:`1 \leq n_\text{max} \leq 200`. The number
+    of points in the sampling grid equals :math:`(n_\text{max} + 1)^2`.
 
     Parameters
     ----------
