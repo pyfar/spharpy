6b577c1e0635a1788dcc32133384e47c090fe178
Anton Hoyer <156099087+hoyer-a@users.noreply.github.com>
2025-12-01 10:03:18 +0100

    initial commit

--- spharpy/plot/spatial.py
+++ spharpy/plot/spatial.py
@@ -537,14 +537,14 @@ def balloon(
 
     Returns
     -------
-    ax : matplotlib.axes.Axes
+    ax : matplotlib.axes.Axes, list[matplotlib.axes.Axes]
         If `colorbar` is ``True`` a list of two axes is returned. The first
         one is the axis on which the data is plotted, the second one is the
         axis of the colorbar. If `colorbar` is ``False``, only the axis on
         which the data is plotted is returned.
     plot : matplotlib.trisurf
         The trisurf object created by the function.
-    cb : matplotlib.colorbar.Colorbar
+    cb : matplotlib.colorbar.Colorbar, None
         The Matplotlib colorbar object if `colorbar` is ``True`` and ``None``
         otherwise. This can be used to control the appearance of the colorbar,
         e.g., the label can be set by ``colorbar.set_label()``.
@@ -939,14 +939,14 @@ def contour_map(
 
     Returns
     -------
-    ax : matplotlib.axes.Axes
+    ax : matplotlib.axes.Axes, list[matplotlib.axes.Axes]
         If `colorbar` is ``True`` a list of two axes is returned. The first
         one is the axis on which the data is plotted, the second one is the
         axis of the colorbar. If `colorbar` is ``False``, only the axis on
         which the data is plotted is returned.
     cf : matplotlib.contour.QuadContourSet
         The contour plot object.
-    cb : matplotlib.colorbar.Colorbar
+    cb : matplotlib.colorbar.Colorbar, None
         The Matplotlib colorbar object if `colorbar` is ``True`` and ``None``
         otherwise. This can be used to control the appearance of the colorbar,
         e.g., the label can be set by ``colorbar.set_label()``.
