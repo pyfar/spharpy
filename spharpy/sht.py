bugfix wrong axis

spharpy/sht.py
@@ -67,7 +67,7 @@ def sht(signal, spherical_harmonics, axis='auto'):
     data = np.moveaxis(data, axis, -2)
 
     # perform transform
-    data_nm = np.tensordot(Y_inv, data, [1, axis])
+    data_nm = np.tensordot(Y_inv, data, [1, -2])
 
     # move SH channels to -2
     data_nm = np.moveaxis(data_nm, 0, -2)

tests/test_sht.py
@@ -120,8 +120,8 @@ def test_sht_auto_axis():
         _ = sht(signal, sh, axis='auto')
 
     signal = pf.Signal(data=np.zeros((64, 64, 64)), sampling_rate=48000)
-    with raises(ValueError, match="Too many axis match the number of spherical "
-                                  "harmonics basis functions"):
+    with raises(ValueError, match="Too many axis match the number of "
+                                  "spherical harmonics basis functions"):
         _ = sht(signal, sh, axis='auto')
 
 