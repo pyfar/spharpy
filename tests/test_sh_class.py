eaa6f7881116da6af889b8ddb41af6d573048697
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2025-12-16 17:52:39 +0100

    add SphericalHarmonics.from_definition and tests thereof

--- tests/test_sh_class.py
+++ tests/test_sh_class.py
@@ -3,8 +3,9 @@ Tests for spherical harmonic class.
 """
 import pytest
 import numpy as np
-from spharpy import SphericalHarmonics
-from spharpy.classes.sh import SphericalHarmonicDefinition
+from spharpy import (SphericalHarmonics, SamplingSphere,
+                     SphericalHarmonicDefinition)
+import deepdiff
 
 
 def test_spherical_harmonics_definition_init():
@@ -182,6 +183,36 @@ def test_sphharm_init_invalid_n_max(icosahedron_sampling):
     """Test error handling for invalid n_max values."""
     with pytest.raises(ValueError, match='n_max must be a positive integer'):
         SphericalHarmonics(n_max=-1, coordinates=icosahedron_sampling)
+
+def test_sphharm_init_from_definition():
+    """Test initialization using SphericalHarmonicDefinition."""
+
+    n_max = 0
+    coordinates = SamplingSphere(1, 0, 0)
+    inverse_method = "auto"
+
+    # generate from definition
+    definition = SphericalHarmonicDefinition(n_max)
+    sh_from_definition = SphericalHarmonics.from_definition(
+        coordinates, definition, inverse_method)
+
+    # generate manually
+    sh_manually = SphericalHarmonics(
+        n_max, coordinates, definition.basis_type, definition.normalization,
+        definition.channel_convention, inverse_method,
+        definition.condon_shortley)
+
+    assert not deepdiff.DeepDiff(
+        sh_from_definition.__dict__, sh_manually.__dict__)
+
+def test_sphharm_init_from_definition_error():
+    """Test error when passing wrong type to `from_definition` class method."""
+
+    message = "definition must be a SphericalHarmonicDefinition"
+
+    with pytest.raises(TypeError, match=message):
+        SphericalHarmonics.from_definition(
+            SamplingSphere(1, 0, 0), "definition")
 
 def test_sphharm_compute_basis(icosahedron_sampling):
     """Test spherical harmonic basis computation."""
