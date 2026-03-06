41f6bf4e33a3bcafa371e43245691a0932331f60
Marco Berzborn <m.berzborn@tue.nl>
2026-03-06 18:58:57 +0100

    Adapt new coordinate creation fixture to test spherical harmonics with pyfar.Coordinates and spharpy.SamplingSphere.

--- tests/test_spherical_harmonics.py
+++ tests/test_spherical_harmonics.py
@@ -5,13 +5,15 @@ import spharpy.spherical as sh
 import numpy as np
 import numpy.testing as npt
 import pytest
+from pyfar import Coordinates
+from spharpy import SamplingSphere
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
 @pytest.mark.parametrize("normalization", ['N3D', 'NM', 'maxN', 'SN3D', 'SNM'])
 @pytest.mark.parametrize("channel_convention", ['ACN', 'FuMa'])
 @pytest.mark.parametrize("condon_shortley", [True, False, 'auto'])
-def test_spherical_harmonic(make_coordinates, implementation,
+def test_spherical_harmonic(make_coordinates, coordinate_object,
                             normalization, channel_convention,
                             condon_shortley):
     n_max = 1
@@ -20,7 +22,7 @@ def test_spherical_harmonic(make_coordinates, implementation,
     rad = np.ones(3, dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     phase_conv_id = 'None' if not condon_shortley else 'Condon-Shortley'
 
@@ -45,11 +47,11 @@ def test_spherical_harmonic(make_coordinates, implementation,
     np.testing.assert_allclose(Y, basis, atol=1e-13)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
 @pytest.mark.parametrize("condon_shortley", [True, False, 'auto'])
 @pytest.mark.parametrize("channel_convention", ['ACN', 'FuMa'])
 @pytest.mark.parametrize("normalization", ['N3D', 'SN3D'])
-def test_spherical_harmonics_real(make_coordinates, implementation,
+def test_spherical_harmonics_real(make_coordinates, coordinate_object,
                                   normalization, channel_convention,
                                   condon_shortley):
     n_max = 1
@@ -58,7 +60,7 @@ def test_spherical_harmonics_real(make_coordinates, implementation,
     rad = np.ones(3, dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     phase_conv_id = 'Condon-Shortley' if condon_shortley is True else 'None'
 
@@ -83,15 +85,15 @@ def test_spherical_harmonics_real(make_coordinates, implementation,
     np.testing.assert_allclose(basis, Y, atol=1e-13)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_invalid_nmax(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonics_invalid_nmax(make_coordinates, coordinate_object):
     n_max = 4
     theta = np.array([np.pi/2, np.pi/2, 0], dtype=float)
     phi = np.array([0, np.pi/2, 0], dtype=float)
     rad = np.ones(3, dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     with pytest.raises(ValueError,
                        match='MaxN normalization is only'
@@ -105,15 +107,15 @@ def test_spherical_harmonics_invalid_nmax(make_coordinates, implementation):
                                          normalization='maxN')
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_invalid_fuma(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonics_invalid_fuma(make_coordinates, coordinate_object):
     n_max = 4
     theta = np.array([np.pi/2, np.pi/2, 0], dtype=float)
     phi = np.array([0, np.pi/2, 0], dtype=float)
     rad = np.ones(3, dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     with pytest.raises(ValueError,
                        match='FuMa channel convention is only'
@@ -127,16 +129,17 @@ def test_spherical_harmonics_invalid_fuma(make_coordinates, implementation):
                                          channel_convention='FuMa')
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_invalid_condon_shortley(make_coordinates,
-                                                     implementation):
+@pytest.mark.parametrize("coordinate_object", [SamplingSphere, Coordinates])
+def test_spherical_harmonics_invalid_condon_shortley(
+        make_coordinates, coordinate_object,
+    ):
     n_max = 4
     theta = np.array([np.pi/2, np.pi/2, 0], dtype=float)
     phi = np.array([0, np.pi/2, 0], dtype=float)
     rad = np.ones(3, dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     with pytest.raises(ValueError,
                        match="Condon_shortley has to be a bool, or 'auto'."):
@@ -148,8 +151,8 @@ def test_spherical_harmonics_invalid_condon_shortley(make_coordinates,
                                          condon_shortley='xx')
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonic_default_n10(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonic_default_n10(make_coordinates, coordinate_object):
     """Test the default parameters of SH basis function generator. This
     simultaneously tests if the methods still match the implementation
     up to spharpy 0.6.2.
@@ -159,7 +162,7 @@ def test_spherical_harmonic_default_n10(make_coordinates, implementation):
     phi = np.array([0, np.pi/2, 0], dtype=float)
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     Y = np.genfromtxt(
         './tests/data/sh_basis_cplx_n10.csv',
@@ -171,9 +174,10 @@ def test_spherical_harmonic_default_n10(make_coordinates, implementation):
     np.testing.assert_allclose(Y, basis, atol=1e-13)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_real_n10_default(make_coordinates,
-                                              implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonics_real_n10_default(
+        make_coordinates, coordinate_object,
+    ):
     """Test the default parameters of SH basis function generator. This
     simultaneously tests if the methods still match the implementation
     up to spharpy 0.6.2.
@@ -184,7 +188,7 @@ def test_spherical_harmonics_real_n10_default(make_coordinates,
     rad = np.ones(4)
 
     coords = make_coordinates.create_coordinates(
-        implementation, rad, theta, phi)
+        coordinate_object, rad, theta, phi)
 
     Y = np.genfromtxt('./tests/data/sh_basis_real.csv',
                       dtype=float,
@@ -193,8 +197,8 @@ def test_spherical_harmonics_real_n10_default(make_coordinates,
     np.testing.assert_allclose(basis, Y, atol=1e-13)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_orthogonality(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_orthogonality(make_coordinates, coordinate_object):
     """
     Check if the orthonormality condition of the spherical harmonics is
     fulfilled.
@@ -205,7 +209,7 @@ def test_orthogonality(make_coordinates, implementation):
     n_points = phi.size
     n_points = len(theta)
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
     basis = sh.spherical_harmonic_basis(n_max, coords)
 
     inner = (basis @ np.conjugate(basis.T))
@@ -215,8 +219,8 @@ def test_orthogonality(make_coordinates, implementation):
         orth, np.ones(n_points, dtype=complex), rtol=1e-10)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_orthogonality_real(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_orthogonality_real(make_coordinates, coordinate_object):
     """
     Check if the orthonormality condition of the reavl valued spherical
     harmonics is fulfilled.
@@ -227,7 +231,7 @@ def test_orthogonality_real(make_coordinates, implementation):
     n_points = phi.size
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     basis = sh.spherical_harmonic_basis_real(n_max, coords)
 
@@ -237,14 +241,16 @@ def test_orthogonality_real(make_coordinates, implementation):
     np.testing.assert_allclose(orth, np.ones(n_points), rtol=1e-10, atol=1e-10)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonic_basis_gradient(make_coordinates, implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonic_basis_gradient(
+        make_coordinates, coordinate_object,
+    ):
     n_max = 15
     theta = np.array([np.pi/2, np.pi/2, 0, np.pi/2, np.pi/4])
     phi = np.array([0, np.pi/2, 0, np.pi/4, np.pi/4])
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     grad_ele, grad_azi = \
         sh.spherical_harmonic_basis_gradient(n_max, coords)
@@ -262,15 +268,16 @@ def test_spherical_harmonic_basis_gradient(make_coordinates, implementation):
     npt.assert_allclose(grad_azi, desire_azi, rtol=1e-10, atol=1e-10)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
 def test_spherical_harmonic_basis_gradient_real(
-        make_coordinates, implementation):
+        make_coordinates, coordinate_object,
+    ):
     n_max = 15
     theta = np.array([np.pi/2, np.pi/2, 0, np.pi/2, np.pi/4])
     phi = np.array([0, np.pi/2, 0, np.pi/4, np.pi/4])
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     grad_ele, grad_azi = \
         sh.spherical_harmonic_basis_gradient_real(n_max, coords)
@@ -288,15 +295,16 @@ def test_spherical_harmonic_basis_gradient_real(
     npt.assert_allclose(grad_azi, desire_azi, rtol=1e-10, atol=1e-10)
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_gradient_invalid_nmax(make_coordinates,
-                                                   implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonics_gradient_invalid_nmax(
+        make_coordinates, coordinate_object,
+    ):
     n_max = 15
     theta = np.array([np.pi/2, np.pi/2, 0, np.pi/2, np.pi/4])
     phi = np.array([0, np.pi/2, 0, np.pi/4, np.pi/4])
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     with pytest.raises(ValueError,
                        match='MaxN normalization is only'
@@ -310,15 +318,16 @@ def test_spherical_harmonics_gradient_invalid_nmax(make_coordinates,
                                                   normalization='maxN')
 
 
-@pytest.mark.parametrize("implementation", ['spharpy', 'pyfar'])
-def test_spherical_harmonics_gradient_invalid_fuma(make_coordinates,
-                                                   implementation):
+@pytest.mark.parametrize("coordinate_object", [Coordinates, SamplingSphere])
+def test_spherical_harmonics_gradient_invalid_fuma(
+        make_coordinates, coordinate_object,
+    ):
     n_max = 15
     theta = np.array([np.pi/2, np.pi/2, 0, np.pi/2, np.pi/4])
     phi = np.array([0, np.pi/2, 0, np.pi/4, np.pi/4])
 
     coords = make_coordinates.create_coordinates(
-        implementation, np.ones_like(theta), theta, phi)
+        coordinate_object, np.ones_like(theta), theta, phi)
 
     with pytest.raises(ValueError,
                        match='FuMa channel convention is only'
