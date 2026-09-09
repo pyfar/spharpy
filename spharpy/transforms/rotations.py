bugfix sh_axis in rotation

spharpy/transforms/rotations.py
@@ -289,14 +289,11 @@ class SphericalHarmonicRotation(Rotation):
         M = np.linalg.multi_dot(list(D)) if D.ndim > 2 else D
 
         data = target._data
-        sh_caxis = target.sh_caxis
-
-        # find non negative axis
-        sh_caxis = np.core.numeric.normalize_axis_index(sh_caxis, data.ndim)
+        sh_caxis = target.caxis_spherical_harmonics - 1
 
         # move SH axis to front
         data = np.moveaxis(data, sh_caxis, 0)
-
+        
         # apply rotation
         rotated_data = np.tensordot(M, data, axes=(1, 0))
 